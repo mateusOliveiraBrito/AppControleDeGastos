28b0f057a68ea09b6b3af2b8a3961503369bbfd8
Mateus Oliveira Brito <mateus.brito@engemail.com.br>
2022-02-01 22:26:15 -0300

    Cria tela principal e implementa tempo de carregamento da tela de carregamento. Além disso, altera o tema das telas no Manifest

--- app/src/main/java/com/mateus/brito/appcontroledegastos/SplashScreen.java
+++ app/src/main/java/com/mateus/brito/appcontroledegastos/SplashScreen.java
@@ -2,7 +2,9 @@ package com.mateus.brito.appcontroledegastos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                abreTelaPrincipal();
+            }
+        }, 2000);
+    }
+
+    private void abreTelaPrincipal() {
+        Intent i = new Intent(this, Principal.class);
+        startActivity(i);
+        finish();
     }
 }
