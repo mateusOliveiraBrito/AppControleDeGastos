863626eeccf3cc1ab3687e917e26cf4ca3421d29
Mateus Oliveira Brito <mateus.brito@engemail.com.br>
2022-02-13 21:59:13 -0300

    Implementa tela de cadastro de nova vigência

---
+++ app/src/main/java/com/mateus/brito/appcontroledegastos/CadastroDeVigencia.java
@@ -0,0 +1,14 @@
+package com.mateus.brito.appcontroledegastos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroDeVigencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_de_vigencia);
+    }
+}
